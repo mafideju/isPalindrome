c6519474462fbae00886b9f426deb017a974e744
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-12-16 22:59:42 -0200

    Correção de Variavel

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
-const isPalindrome = (string) => {
-  string = string.toLowerCase()
-  const charArr = string.split('')
-  const validChar = 'abcdefghijklmnopqrstuwxyz'.split()
+const isPalindrome = (sentenca) => {
+  sentenca = sentenca.toLowerCase();
+  const arrayDeLetrasOrigem = sentenca.split('');
+  const letrasValidas = 'abcdefghijklmnopqrstuwxyz'.split('');
 
-  let letterArr = []
-  charArr.forEach(char => {
-    if (validChar.indexOf(char) > -1) letterArr.push(char)
+  // console.log('arrayDeLetrasOrigem:', arrayDeLetrasOrigem)
+  // console.log('letrasValidas', letrasValidas)
+
+  let arrayDeLetrasDestino = [];
+  arrayDeLetrasOrigem.forEach(letra => {
+    if (letrasValidas.indexOf(letra) > -1) {
+      return arrayDeLetrasDestino.push(letra);
+    }
   });
 
-  if (letterArr.join('') === letterArr.reverse().join('')) return true;
-  else return false
+  // console.log('letrasDestino =', arrayDeLetrasDestino)
+  // console.log('join()', arrayDeLetrasDestino.join(''));
+  // console.log('reverse().join()', arrayDeLetrasDestino.reverse().join(''));
+
+  if (arrayDeLetrasDestino.join('') === arrayDeLetrasDestino.reverse().join('')) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
-const isPalindromeTest = isPalindrome('socorram me subi no onibus em marrocos')
-console.log(isPalindromeTest)
+const isPalindromeTest = isPalindrome('socorram me subi no onibus em marrocos');
+console.log(isPalindromeTest);
+
+//socorram me subi no onibus em marrocos
